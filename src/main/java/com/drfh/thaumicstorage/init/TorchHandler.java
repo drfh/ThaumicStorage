4d2fa12af18c68f5de5cc0a92b9e639be51f8910
David Harris <DavidRFHarris@gmail.com>
2016-01-03 22:42:47 -0500

    Aura Consumption for torches
    This code contains the torch to aura conversion for placing torches.

--- src/main/java/com/drfh/thaumicstorage/init/TorchHandler.java
+++ src/main/java/com/drfh/thaumicstorage/init/TorchHandler.java
@@ -4,11 +4,12 @@ import com.drfh.thaumicstorage.common.items.ArcaneTorchDispenser;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import thaumcraft.api.aspects.Aspect;
+import thaumcraft.api.aura.AuraHelper;
 
 public class TorchHandler
 {
@@ -20,35 +21,32 @@ public class TorchHandler
 	public void playerInteractEventHandler(PlayerInteractEvent event)
 	{
 
-		if (processingEvent | eventNotRelevant(event))
+		if(processingEvent|eventNotRelevant(event))
 			return;
 
-		ItemStack itemInHand = event.entityPlayer.inventory.getCurrentItem();
+		ItemStack	itemInHand=event.entityPlayer.inventory.getCurrentItem();
 
 		if(notHoldingDispenser(itemInHand))
 			return;
-		if (!targetBlockAcceptsTorches(event))
+		if(!targetBlockAcceptsTorches(event))
 			return;
 		
-/*		InventoryPlayer inventory = event.entityPlayer.inventory;
-		int mainInventoryTorchSlotIndex = getTorchSlotIndex(inventory.mainInventory);
+		if(AuraHelper.getAura(event.world,event.pos,Aspect.AIR)>0&&AuraHelper.getAura(event.world,event.pos,Aspect.AIR)>0)
+		{
+			AuraHelper.drainAuraAvailable(event.world, event.pos, Aspect.AIR,1);
+			AuraHelper.drainAuraAvailable(event.world, event.pos, Aspect.FIRE,1);
+			//	Maybe take some more randomly to express inefficiency.
+		}
+		
+		ItemStack	torchStack=new ItemStack(Blocks.torch,1);
 
-		if (mainInventoryTorchSlotIndex == NOTORCHESFOUND)
-			return;
-		ItemStack torchStack = inventory.mainInventory[mainInventoryTorchSlotIndex];
-*/		ItemStack torchStack = new ItemStack(Blocks.torch,1);
-
-		processingEvent = true;
-		useItem(event, torchStack);
-
-//		if (torchStack.stackSize == 0)
-//			inventory.mainInventory[mainInventoryTorchSlotIndex] = null;
+		processingEvent=true;
+		useItem(event,torchStack);
 
 		event.entityPlayer.openContainer.detectAndSendChanges();
-		processingEvent = false;
+		processingEvent=false;
 		event.setCanceled(true);
 	}
-
 
 	private void useItem(PlayerInteractEvent event, ItemStack torchStack)
 	{
@@ -57,11 +55,9 @@ public class TorchHandler
 				event.face, 0.5f, 0.5f, 0.5f);
 	}
 
-
 	private boolean targetBlockAcceptsTorches(PlayerInteractEvent event)
 	{
-		// Unless I'm having a serious brain fart, it seems that .canPlaceTorchOnTop() should be a static method from the Block class
-		Block block = event.world.getBlockState(event.pos).getBlock();
+		Block	block=event.world.getBlockState(event.pos).getBlock();
 		return block.canPlaceTorchOnTop(event.world, event.pos);
 
 	}
@@ -69,8 +65,7 @@ public class TorchHandler
 
 	private boolean eventNotRelevant(PlayerInteractEvent event)
 	{
-		return event
-				.isCanceled() || event.world.isRemote || event.action != PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK;
+		return event.isCanceled() || event.world.isRemote || event.action != PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK;
 	}
 
 
